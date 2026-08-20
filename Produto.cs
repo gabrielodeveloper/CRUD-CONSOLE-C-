a8cb4b9281619eb27da7df6897e5681d72fe6930
unknown <gabriel0211oliveira15@gmail.com>
2026-08-19 22:55:09 -0300

    refactor: separar responsabilidades de validação de produtos

--- Produto.cs
+++ Produto.cs
@@ -2,7 +2,7 @@ namespace PrimeiroProjeto;
 public class Produto
 {
     public int Codigo { get; set; }
-    public string? Descricao { get; set; }
+    public string Descricao { get; set; } = string.Empty;
     public int Estoque { get; set; }
 
     public Decimal Preco { get; set; }
